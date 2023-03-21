e8e3d6e6e8007eaedfa3fb412a3dfee6a64ea100
Lisa Walters <lisacampiwalters@gmail.com>
2023-03-21 00:11:35 -0600

    created three car objects

---
+++ CarLotSimulatorApp/CarLotSimulator/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car() {
+        }
+        public string Name { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+
+        public string HonkNoise { get; set; }
+
+        public bool IsDriveable { get; set; }
+
+        public void makeEngineNoise (string EngineNoise)
+        {
+            Console.WriteLine($"{EngineNoise}");
+        }
+
+        public void makeHonkNoise(string HonkNoise)
+        {
+            Console.WriteLine($"{HonkNoise}");
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -28,6 +28,14 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
-        }
+
+            var highlander = new Car();
+            var jeep = new Car();
+            var mazda = new Car();
+        
+        
+        
+        
+        }// End main method
     }
 }
